3cb119caa3f2480f4908e4c35cd8726d8b459b89
Bagas <bagashyt@gmail.com>
2024-09-15 20:15:28 +0700

    user service

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,8 +1,34 @@
 package api
 
-import "database/sql"
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/bagashyt/ecom/service/user"
+	"github.com/gorilla/mux"
+)
 
 type APIServer struct {
-	add string
-	db  *sql.DB
+	addr string
+	db   *sql.DB
+}
+
+func NewAPIServer(addr string, db *sql.DB) *APIServer {
+	return &APIServer{
+		addr: addr,
+		db:   db,
+	}
+}
+
+func (s *APIServer) Run() error {
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	userHandler := user.NewHandler()
+	userHandler.RegisterRoutes(subrouter)
+
+	log.Println("Listening on", s.addr)
+
+	return http.ListenAndServe(s.addr, router)
 }
