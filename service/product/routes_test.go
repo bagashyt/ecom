4c03572512cdcaae94cd8e6f00512596c2aff609
Bagas <bagashyt@gmail.com>
2024-09-29 00:18:18 +0700

    testing product routes

--- service/product/routes_test.go
+++ service/product/routes_test.go
@@ -1,6 +1,8 @@
 package product
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -16,7 +18,7 @@ func TestProductServiceHandlers(t *testing.T) {
 	handler := NewHandler(productStore, userStore)
 
 	t.Run("should handle get products", func(t *testing.T) {
-		req, err := http.NewRequest(http.MethodGet, "/product", nil)
+		req, err := http.NewRequest(http.MethodGet, "/products", nil)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -30,6 +32,72 @@ func TestProductServiceHandlers(t *testing.T) {
 
 		if rr.Code != http.StatusOK {
 			t.Errorf("expected status code %d, got %d", http.StatusOK, rr.Code)
+		}
+	})
+
+	t.Run("should fail if the product ID is not a number", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, "/products/abc", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/products/{productID}", handler.handleGetProduct).Methods(http.MethodGet)
+		router.ServeHTTP(rr, req)
+
+		if rr.Code != http.StatusBadRequest {
+			t.Errorf("expected status code %d, got %d", http.StatusBadRequest, rr.Code)
+		}
+	})
+
+	t.Run("should fail creating a product if the payload is missing", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodPost, "/products", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/products", handler.handleCreateProduct).Methods(http.MethodPost)
+
+		router.ServeHTTP(rr, req)
+
+		if rr.Code != http.StatusBadRequest {
+			t.Errorf("expected status code %d, got %d", http.StatusBadRequest, rr.Code)
+		}
+	})
+
+	t.Run("should handle creating a product", func(t *testing.T) {
+		payload := types.CreateProductPayload{
+			Name:        "test",
+			Price:       100,
+			Image:       "test.jpg",
+			Description: "test description",
+			Quantity:    10,
+		}
+
+		marshalled, err := json.Marshal(payload)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req, err := http.NewRequest(http.MethodPost, "/products", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/products", handler.handleCreateProduct).Methods(http.MethodPost)
+
+		router.ServeHTTP(rr, req)
+
+		if rr.Code != http.StatusCreated {
+			t.Errorf("expected status code %d, got %d", http.StatusCreated, rr.Code)
 		}
 	})
 }
